8395050842f49bccdd663553032464897c770fdc
AdeSetiawan12 <142704611+AdeSetiawan12@users.noreply.github.com>
2023-09-28 22:27:30 +0700

    update malam

---
+++ PROJECT-AKHIR/PROJECT-AKHIR/Js/script.js
@@ -0,0 +1,70 @@
+$(document).on('ready', function() {
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+     
+            ]
+          });
+        
+      })
+
+      $(".second").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 8,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+     
+            ]
+          });
+        
+      
